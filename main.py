63f77b2b630b0ec8a90253a6673388b81f54506e
jckane5187 <jckane5187@gmail.com>
2025-12-23 16:00:08 -0600

    added stats.py, moved get_num_words function to stats.py and imported function to main

--- main.py
+++ main.py
@@ -2,10 +2,7 @@ def get_book_text(path):
     with open(path) as f:
         return f.read()
 
-
-def get_num_words(text):
-    words = text.split()
-    return len(words)
+from stats import get_num_words
 
 def main():
     book_path = "books/frankenstein.txt"
