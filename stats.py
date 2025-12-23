db45d2744f2b499ac674a31f628df8fa07daec77
jckane5187 <jckane5187@gmail.com>
2025-12-23 16:25:44 -0600

    added get_num_chars to stats and imported to main

--- stats.py
+++ stats.py
@@ -1,3 +1,16 @@
 def get_num_words(text):
     words = text.split()
     return len(words)
+
+def get_num_chars(text):
+    char_count = {}
+
+    for character in text:
+        char = character.lower()
+        if char not in char_count:
+            char_count[char] = 1
+        else:
+            char_count[char] += 1
+
+    return char_count
+
