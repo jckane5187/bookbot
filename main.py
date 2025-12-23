5de9463716a140dca59e1d0a40751e1ea8b698d5
jckane5187 <jckane5187@gmail.com>
2025-12-22 19:18:26 -0600

    added .gitignore, updated main.py with two functions that get the book's text and counts the number of words

---
+++ main.py
@@ -0,0 +1,16 @@
+def get_book_text(path):
+    with open(path) as f:
+        return f.read()
+
+
+def get_num_words(text):
+    words = text.split()
+    return len(words)
+
+def main():
+    book_path = "books/frankenstein.txt"
+    text = get_book_text(book_path)
+    num_words = get_num_words(text)
+    print(f"Found {num_words} total words")
+
+main()
