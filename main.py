78089e233b028a11a82354ea5923366203b34a21
jckane5187 <jckane5187@gmail.com>
2025-12-24 15:04:04 -0600

    replaced hard coded book path with sys.argv input

--- main.py
+++ main.py
@@ -5,7 +5,11 @@ def get_book_text(path):
 from stats import get_num_words, get_num_chars, sorted_chars_list
 
 def main():
-    book_path = "books/frankenstein.txt"
+    import sys
+    if len(sys.argv) < 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    book_path = sys.argv[1]
     text = get_book_text(book_path)
     num_words = get_num_words(text)
     num_chars = get_num_chars(text)
