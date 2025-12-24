3122b0f85254a8528137c00cf042b4d095a61275
jckane5187 <jckane5187@gmail.com>
2025-12-23 18:44:37 -0600

    added sorted_chars_list to stats and imported to main, updated main to print a report of words and characters

--- main.py
+++ main.py
@@ -2,14 +2,24 @@ def get_book_text(path):
     with open(path) as f:
         return f.read()
 
-from stats import get_num_words, get_num_chars
+from stats import get_num_words, get_num_chars, sorted_chars_list
 
 def main():
     book_path = "books/frankenstein.txt"
     text = get_book_text(book_path)
     num_words = get_num_words(text)
     num_chars = get_num_chars(text)
+    sorted = sorted_chars_list(text)
+    
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {book_path}...")
+    print("----------- Word Count ----------")
     print(f"Found {num_words} total words")
-    print(num_chars)
+    print("--------- Character Count -------")
+    for char_set in sorted:
+        character = char_set["char"]
+        if character.isalpha():
+            print(f"{char_set["char"]}: {char_set["num"]}")
+    print("============= END ===============")
 
 main()
--- stats.py
+++ stats.py
@@ -14,3 +14,19 @@ def get_num_chars(text):
 
     return char_count
 
+def sort_on(items):
+    return items["num"]
+
+def sorted_chars_list(text):
+    unsorted_chars = get_num_chars(text)
+    sorted_chars = []
+
+    for character in unsorted_chars:
+        temp_dict = {}
+        temp_dict["char"] = character
+        temp_dict["num"] = unsorted_chars[character]
+        sorted_chars.append(temp_dict)
+    
+    sorted_chars.sort(reverse=True, key=sort_on)
+
+    return sorted_chars
